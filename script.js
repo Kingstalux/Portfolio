e779c0a390ac68b7458671fdd52ae5a84efdb25e
Kingstalux <kingsleyngu25@gmail.com>
2021-08-30 21:50:24 +0100

    Edit script.js file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function myFunction() {
+const myFunction = () => {
   document.getElementById('drop-menu').classList.add('show');
 }
 
-function myFunction1() {
+const myFunction1 = () => {
   document.getElementById('drop-menu').classList.remove('show');
 }
