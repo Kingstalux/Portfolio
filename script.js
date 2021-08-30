ca3b88b9bd9e848244d70beb93f37eef7feb5667
Kingstalux <kingsleyngu25@gmail.com>
2021-08-30 20:58:33 +0100

    Edit linters errors and readme file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function myFunction() {
-  document.getElementById("drop-menu").classList.add("show");
+  document.getElementById('drop-menu').classList.add('show');
 }
 
 function myFunction1() {
-  document.getElementById("drop-menu").classList.remove("show");
+  document.getElementById('drop-menu').classList.remove('show');
 }
