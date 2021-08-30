51e7604ebe7c88a29c49b596f03d6418a10cde80
Gordon <gordon.tinyefuza@gmail.com>
2021-08-30 21:22:44 +0300

    add onclick events on the menu and cancel icons

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  document.getElementById("drop-menu").classList.add("show");
+}
+
+function myFunction1() {
+  document.getElementById("drop-menu").classList.remove("show");
+}
