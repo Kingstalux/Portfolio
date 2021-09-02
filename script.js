e9fe577f0d36089da467f45e72ccf9df1bc43463
Kingstalux <kingsleyngu25@gmail.com>
2021-09-02 20:00:53 +0100

    Add projects to desktop container

--- script.js
+++ script.js
@@ -47,6 +47,56 @@ const mobile_container =  [
   }
 ]
 
+const projects_container = [
+  p1 = {
+    title: "Multi-Post Stories",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    tech: ["css", "html", "bootstrap", "Ruby"],
+    "image desktop": "images/Img.png",
+    "image mobile": "images/image1.png",
+    button: "See Project",
+    "link to live version": "#1",
+    "link to source": "#2"
+  },
+
+  p2 = {
+    title: "Professional Art Printing Data",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    tech: ["html", "bootstrap", "Ruby"],
+    button: "See Project",
+    "link to live version": "#3",
+    "link to source": "#4"
+  },
+
+  p3 = {
+    title: "Data Dashboard Healthcare",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    tech: ["html", "bootstrap", "Ruby"],
+    button: "See Project",
+    "link to live version": "#5",
+    "link to source": "#6"
+  },
+
+  p4 = {
+    title: "Website Portfolio",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    tech: ["html", "bootstrap", "Ruby"],
+    button: "See Project",
+    "link to live version": "#7",
+    "link to source": "#8"
+  },
+
+  p5 = {
+    title: "Keeping track of hundreds of components website",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    tech: ["html", "Bootstrap", "Ruby on rails"],
+    "live button": "See live",
+    "source button": "See Source",
+    "image desktop": "images/Snapshoot.png"
+  }
+  
+]
+
 
 
 
